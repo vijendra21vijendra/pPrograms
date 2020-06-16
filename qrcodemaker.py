a36621ba5eaadb9b216fdb9e36674364f8105bb0
Vijendra singh shekhawat <sikarvijendrasingh@gmail.com>
2020-06-16 10:12:51 +0530

    qrcodemaker

---
+++ qrcodemaker.py
@@ -0,0 +1,3 @@
+import qrcode
+img = qrcode.make("my name is vijendra singh and my num is 9079999751")
+img.save("myinf.png")
--- qrreaderandmaker.py
+++ qrreaderandmaker.py
@@ -1,5 +1,6 @@
 from pyzbar.pyzbar import decode
 from PIL import Image
-d = decode(Image.open("./qcodefile.png"))
+# d = decode(Image.open("./qcodefile.png"))
+d = decode(Image.open("./myinf.png"))
 print(d[0].data.decode())
 print(type(d))
